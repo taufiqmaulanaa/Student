c2a0715334dbc05c0707475469437255d2a3be22
taufiqmaulanaa <taufiqmaulanaa@gmail.com>
2017-06-08 17:55:51 +0700

    build

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['as'=>'/','uses'=>'LoginController@getLogin']);
+Route::post('/login',['as'=>'login','LoginController@postLogin']);
 
-Route::get('/dashboard',function(){
-	return view('layout.master');
-});
+Route::get('logout',['as'=>'logout','LoginController@getLogout']);
+
+Route::group(['middleware'=>['authen','roles']],function(){
+	Route::get('/dashboard', ['as'=>'dashboard','uses'=>'DashboardController@dashboard']);
+})
